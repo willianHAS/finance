0542fb0834c6e6a7dd7030a17b9a8d96422f8ceb
Willian Marchiori <33064642+willianHAS@users.noreply.github.com>
2025-01-28 10:34:20 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,76 @@
 import 'package:flutter/material.dart';
-import 'screens/finance_home_page.dart'; // Importe a tela principal
+import 'models/transaction.dart';
+import 'widgets/transaction_list.dart';
+import 'widgets/new_transaction.dart';
+import 'widgets/transaction_chart.dart';
 
-void main() {
-  runApp(FinanceApp());
-}
+void main() => runApp(FinanceApp());
 
 class FinanceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Finance App',
+      title: 'App de Finanças',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
       ),
-      home: FinanceHomePage(), // Tela inicial
+      home: FinanceHomePage(),
+    );
+  }
+}
+
+class FinanceHomePage extends StatefulWidget {
+  @override
+  _FinanceHomePageState createState() => _FinanceHomePageState();
+}
+
+class _FinanceHomePageState extends State<FinanceHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Supermercado',
+      amount: 50.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      amount: 80.75,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App de Finanças'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TransactionChart(_userTransactions),
+          NewTransaction(_addNewTransaction),
+          Expanded(
+            child: TransactionList(_userTransactions),
+          ),
+        ],
+      ),
     );
   }
 }
